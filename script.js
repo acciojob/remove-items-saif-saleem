cc2ed5a2c7ff24ea916e8424982a68a8ea3cac52
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-21 00:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let btn = document.getElementsByTagName("input")[0]; // Get the first input element
+let clrsl = document.getElementById("colorSelect");
+
+btn.addEventListener("click", () => {
+  let cv = clrsl.querySelector("option");
+  if (cv) {
+    cv.remove();
+  }
+});
